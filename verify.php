e37d6dac67b5a394a83460a26b6d3e1e5cec049f
Sawaphob Chavana <sawaphob@hotmail.com>
2018-03-20 00:06:27 +0700

    bugfix

--- verify.php
+++ verify.php
@@ -1,6 +1,5 @@
 <?php
-$access_token = '
-CdsWkJVodsaQsGRkKV8y7rFBv8VfW8BCeT2L+taq6mnAx9FjWckEyxx9uAiEf+IvvM83CjInzpkM2ROCShELsy11nWIGUYUdXpo5XB9nR0cUWNbC33dw0mxBegtVn86mK3GqdibWQ1XBfMAgcIQzvAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'CdsWkJVodsaQsGRkKV8y7rFBv8VfW8BCeT2L+taq6mnAx9FjWckEyxx9uAiEf+IvvM83CjInzpkM2ROCShELsy11nWIGUYUdXpo5XB9nR0cUWNbC33dw0mxBegtVn86mK3GqdibWQ1XBfMAgcIQzvAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
